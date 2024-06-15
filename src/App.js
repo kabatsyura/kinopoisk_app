b603c17364e5d5a8db51becd65dcf811c9738183
Дмитрий Кабацюра <cauliflb@student.21-school.ru>
2024-06-16 01:31:50 +0300

    0.1 App shows movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    // eslint-disable-next-line no-unused-vars
+    const {
+      data: { 
+        data: { movies },
+      },
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=year');
+    
+    this.setState({ 
+      movies: movies,
+      isLoading: false
+    });
+
+    console.log(this.state.movies);
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+    <section class="container">
+      {isLoading 
+        ? <div class="loader">
+          <span class="loader__text">Загрузка</span>
+        </div> : movies.map(movie => {
+          console.log(movie);
+          return (
+            <div class="movies">
+            <Movie 
+              key={movie.key}
+              id={movie.id} 
+              title={movie.title} 
+              summary={movie.summary} 
+              rating={movie.rating} 
+              year={movie.year} 
+              poster={movie.medium_cover_image}/>
+            </div>
+          );
+        })}
+    </section>
+    );
+  }
+};
 
 export default App;
